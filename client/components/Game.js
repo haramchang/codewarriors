8b5d3b251cbe6d9de0250413df3881c0eb20b823
Haram Chang <harammchang@gmail.com>
2021-12-15 18:10:52 -0500

    two text editors show

--- client/components/Game.js
+++ client/components/Game.js
@@ -1,26 +1,13 @@
 import React from "react"
 import { connect } from "react-redux"
-import CodeEditor from '@uiw/react-textarea-code-editor';
+import { IDE } from "./IDE"
 
 export const Game = () => {
-  const [code, setCode] = React.useState(
-    `function add(a, b) {\n  return a + b;\n}`
-  );
   return (
     <div id="game-container">
-      This is the game container
-      <CodeEditor
-        value={code}
-        language="js"
-        placeholder="Please enter JS code."
-        onChange={(evn) => setCode(evn.target.value)}
-        padding={15}
-        style={{
-          fontSize: 12,
-          backgroundColor: "#f5f5f5",
-          fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-        }}
-      />
+      This is what shows when someone or 2 players create/join a game
+      <IDE />
+      <IDE />
     </div>
   )
 }
