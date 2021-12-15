06f1ee8cea8e4cae8cd9d7c17ae6e2c6ca05e0af
Haram Chang <harammchang@gmail.com>
2021-12-15 18:47:49 -0500

    implemented sockets server side

--- client/components/Game.js
+++ client/components/Game.js
@@ -1,6 +1,13 @@
 import React from "react"
 import { connect } from "react-redux"
 import { IDE } from "./IDE"
+import { io } from "socket.io-client"
+
+
+const socket = io()
+socket.on("connect", (socket) => {
+  console.log("HELLO???")
+})
 
 export const Game = () => {
   return (
---
+++ server/socket/socket.js
@@ -0,0 +1,12 @@
+const http = require("http")
+const socket = require("socket.io")
+const app = require("../app.js")
+
+const server = http.createServer(app)
+const io = socket(server)
+
+io.on("connection", (socket) => {
+  console.log("New client connected!")
+})
+
+module.exports = server
