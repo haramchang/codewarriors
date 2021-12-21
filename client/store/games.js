6a1495bf55de9c0bc1e1a56a83be33410052fb6a
Haram Chang <harammchang@gmail.com>
2021-12-21 14:18:28 -0500

    thunk for adding a game

--- client/store/games.js
+++ client/store/games.js
@@ -6,13 +6,29 @@ import history from '../history'
  * ACTION TYPES
  */
 const SET_GAME = 'SET_GAME'
+const ADD_GAME = "ADD_GAME"
 
 /**
  * ACTION CREATORS
  */
 const setGame = (game) => ({ type: SET_GAME, game })
 
+const _addGame = (game) => ({
+  type: ADD_GAME,
+  game
+})
+
 /**
  * THUNK CREATORS
  */
+
+export const addGame = () => async dispatch => {
+  try {
+    const game = await axios.post("/")
+    dispatch(_addGame(game))
+  } catch (error) {
+    console.log("error from adding game", error)
+  }
+}
+
 
